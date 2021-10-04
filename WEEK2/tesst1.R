0eeb270ed8af0bee48028c0f12190f146f6ee2f1
BaeEunGi <withjust7@naver.com>
2021-10-04 18:36:19 +0900

    WEEK2

--- WEEK2/tesst1.R
+++ WEEK2/tesst1.R
@@ -1,1 +1,67 @@
-test1
+a = 1
+a=2
+32%%16/3a<-2
+a<-2
+a = 1
+a = 1
+a=2
+1+1
+2+2
+8/3
+32%%16
+32%/%16
+x <- 10
+x
+print(x)
+sum(1,2,3,4,5)
+s <- sum(x,a)
+s
+a = c(1,2,3,4,5,6)
+a
+a[1]
+a[5]
+height <- 177.5
+height
+coffee_price <- 4000
+coffee_price
+sales <- coffee_price *30
+sales
+JD_gender <- "M"
+JD_coffee_choice <- 'Vanila Latte'
+JD_gender
+JD_coffee_choice
+score <- 91
+is_A <- score >= 90
+is_A
+x <- 1:20
+x
+x <- c(1:20)
+x
+ID <- c(1:15)
+ID
+name <- c("Harry", "sam", "Susan")
+name
+has_Laptop <- c(T,F,t)
+has_Laptop
+has_Laptop <- c(T,F,T)
+has_Laptop
+ID[-1]
+ID[-2]
+ID[0]
+ID[1]
+a<-2
+a
+a[0]
+a[1]
+coffee_price <- c("Americano", "Cappuccino", "Espresso)
+coffee_price
+coffee_price <- c("Americano", "Cappuccino", "Espresso")
+coffee_price <- c("Americano", "Cappuccino", "Espresso")
+coffee_price
+coffee_price[1]
+seq(2,20, 2)
+rep(2,10)
+rep(2,100)
+rep(x = c(T,F,T,F),2)
+x
+rep(c(T,F,T,F),2)
